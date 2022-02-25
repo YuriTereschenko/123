86e380e1fda614d855835c50bf9435edd9923207
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-02-25 20:11:27 +0500

    task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a *(-1);
+/* Console.WriteLine(b); */
+if (a < b)
+    {
+        while (a <= b)
+         {
+            Console.Write(a+ " ");
+            a = a+1;
+         }
+    }
+else
+    {
+        while (b <= a)
+         {
+            Console.Write(b+ " ");
+            b = b+1;
+         }
+    }
