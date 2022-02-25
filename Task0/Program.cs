da279d4e8f5c9e6ab725a69d7b7a67df0314537f
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-02-25 18:33:04 +0500

    init commit

---
+++ Task0/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new int();
+Console.WriteLine("input number");
+number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number+"^2= ");
+number = number * number;
+Console.WriteLine(number);
