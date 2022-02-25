98d457c7fc7e5986185ddb87302bf50f7303e71a
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-02-25 19:30:48 +0500

    Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿int A = new int();
+Console.WriteLine("Введите первое число");
+A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A==0 & B != 0)
+{
+    Console.WriteLine("Второе число не является квадратом первого");
+}
+else if (A==0 & B == 0)
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else if (B/A == A) 
+{
+    Console.WriteLine("Второе число является квадратом первого");
+}
+else
+{
+    Console.WriteLine("Второе число не является квадратом первого");
+}
